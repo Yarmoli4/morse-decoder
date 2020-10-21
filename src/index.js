438c5d548baa7fd831cb16cbb1b8baebdd826fdd
Yarmoli4 <70720360+Yarmoli4@users.noreply.github.com>
2020-10-21 12:35:59 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -35,10 +35,13 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    '**********': ' '
 };
 
 function decode(expr) {
-    // write your solution here
+    let decoded = expr.match(/.{1,10}/g);
+    decoded = decoded.map(el => el.replace(/11/g, '-').replace(/10/g, '.').replace(/0/g, ''));
+    return decoded.map(letter=>MORSE_TABLE[letter]).join('');
 }
 
 module.exports = {
